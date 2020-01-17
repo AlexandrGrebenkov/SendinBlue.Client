b3f97ee1ceb2145aecfc3206c54c5afc2682da1b
Alexandr Grebenkov <aleksander.grebenkov@saritasa.com>
2020-01-17 11:53:25 +0500

    Add console app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SendinBlue.Client;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var client = new SendinBlueClient(configuration.GetSection("SendinBlue:ApiKey").Value);
+
+            var attributes = await client.GetContactAttributesAsync(CancellationToken.None);
+            Console.ReadLine();
+        }
+    }
+}
